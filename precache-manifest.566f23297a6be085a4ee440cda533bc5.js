4b8bdbee8cac333ae0ec923a277d2b3e8ca1cfb5
Ency-Ch <n.charumbira@hotmail.co.uk>
2021-12-19 05:11:55 +0000

    Updates

---
+++ precache-manifest.566f23297a6be085a4ee440cda533bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567f57482c9336cd96f13f2ef5f1abb3",
+    "url": "/nc-news/index.html"
+  },
+  {
+    "revision": "6c6a3d5c6649e3d3517f",
+    "url": "/nc-news/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "ba06cff7842b6b3d8a0f",
+    "url": "/nc-news/static/css/main.8cff3a35.chunk.css"
+  },
+  {
+    "revision": "6c6a3d5c6649e3d3517f",
+    "url": "/nc-news/static/js/2.6691d6f1.chunk.js"
+  },
+  {
+    "revision": "2aa2c05c2239026a2a1fbfdd082d1cae",
+    "url": "/nc-news/static/js/2.6691d6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba06cff7842b6b3d8a0f",
+    "url": "/nc-news/static/js/main.c5fd1ea1.chunk.js"
+  },
+  {
+    "revision": "b31e374c2a523692e891",
+    "url": "/nc-news/static/js/runtime-main.afc42a0d.js"
+  }
+]);
