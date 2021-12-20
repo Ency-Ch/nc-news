68b54a919f02551f00bc47d2f7185479a1264139
Ency-Ch <n.charumbira@hotmail.co.uk>
2021-12-20 04:04:48 +0000

    Updates

---
+++ precache-manifest.6f7aeb3149c9f34a36f39c0e4c767a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ff3906a81fe6c4afc653c2236f217d",
+    "url": "/nc-news/index.js/index.html"
+  },
+  {
+    "revision": "6c6a3d5c6649e3d3517f",
+    "url": "/nc-news/index.js/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "7070727a4fd96c35921c",
+    "url": "/nc-news/index.js/static/css/main.cdb16d16.chunk.css"
+  },
+  {
+    "revision": "6c6a3d5c6649e3d3517f",
+    "url": "/nc-news/index.js/static/js/2.6691d6f1.chunk.js"
+  },
+  {
+    "revision": "2aa2c05c2239026a2a1fbfdd082d1cae",
+    "url": "/nc-news/index.js/static/js/2.6691d6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7070727a4fd96c35921c",
+    "url": "/nc-news/index.js/static/js/main.198777d0.chunk.js"
+  },
+  {
+    "revision": "7507c50d2fe1b47beeb4",
+    "url": "/nc-news/index.js/static/js/runtime-main.ee5cb028.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nc-news/precache-manifest.ae423669a80763d903bc2840e68cd7bc.js"
+  "/nc-news/index.js/precache-manifest.6f7aeb3149c9f34a36f39c0e4c767a8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nc-news/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nc-news/index.js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
